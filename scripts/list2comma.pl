cf004c3b0e2bb101045215dfe1b398a90afd519f
bmoore <barry.moore@genetics.utah.edu>
2021-06-28 22:45:31 +0000

    Adding support for input file or stream scripts/list2comma.pl

--- scripts/list2comma.pl
+++ scripts/list2comma.pl
@@ -11,11 +11,14 @@ my $usage = "
 Synopsis:
 
 cat list.txt | list2comma.pl
+list2comma.pl list.txt # One record per line in text file.
+list2comma.pl list.tsv # Use first column in TSV
+cut -f 2 list.tsv | list2comma.pl # Use second column of TSV
 
 Description:
 
-Convert a stream of text records (one per line) into a comma separated
-list.
+Convert a stream of text records into a comma separated list.  If the
+text records are from a tab delimited file, the first column is used.
 
 ";
 
@@ -26,11 +29,22 @@ my $opt_success = GetOptions('help'    => \$help,
 die $usage if ! $opt_success;
 print $usage if $help;
 
+my $file = shift @ARGV;
+
+my $IN;
+if (! -t STDIN) {
+    open ($IN, "<&=STDIN") or die "Can't open STDIN\n";
+}
+else {
+    open ($IN, '<', $file) or die "Can't open $file for reading:\n$!\n";
+}
+
 my @list;
 LINE:
-while (my $line = <>) {
+while (my $line = <$IN>) {
     chomp $line;
-    push @list, $line;
+    my @cols = split /\t/, $line;
+    push @list, $cols[0];
 }
 
 print join ',', @list;
